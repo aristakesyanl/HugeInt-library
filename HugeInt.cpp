84257b9ae26bb53748629c2274cb682c1fc7f988
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-03-28 17:59:41 +0400

    Update HugeInt.cpp

--- HugeInt.cpp
+++ HugeInt.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
 #include<string>
 #include<stdexcept>//for invalid argument exception class
+#include<algorithm>
+#include<vector>
 #include"HugeInt.h"
 using namespace std;
 
 
-HugeInt::HugeInt() : length(0), digits(0, 0), sign(0) {}
+HugeInt::HugeInt() : length(0), digits(""), sign(0) {}
 
 HugeInt::HugeInt(int value) {
 	length = 0;
 	if (value < 0) sign = 1;
 	else sign = 0;
 	value = abs(value);
+	if(value==0){
+		digits="0";
+		sign=0;
+		length=1;
+	}
 	while (value > 0) {
-		digits.push_back(value % 10);
+		digits.push_back(char(value % 10+'0'));
 		value /= 10;
 		length++;
 	}
@@ -23,45 +30,63 @@ HugeInt::HugeInt(string value) {
 	//check if value is can be assigned as string
 	//the first charcter should be either '-' or number from 1 to 9
 	int len = value.length();
-	if (len == 0) {
+	
 
-	}
-
-	else if (value[0] != '-') {
+	if (value[0] != '-') {
 
 		sign = 0;
 
-		if ((value[0] - '0' > 10) || (value[0] - '0') < 0) {
-			throw invalid_argument("invalid integer type");
+		//first digits if value should not be '0'
+		if (((value[0] - '0' > 10) || (value[0] - '0') < 1) && value.length()!=1) {
+			throw invalid_argument("Invalid Integer Type: Leading Zero");
 		}
-		length = value.length();
 
-		for (int i = len - 1; i >= 0; i--) {
-			digits.push_back(value[i] - '0');
+		//following digits  should be between 0 to 9
+		for(int i=1; i<len; i++){
+			if((value[i]-'0'>10) || (value[i]-'0')<0){
+				throw invalid_argument("Invalid Integer Type:Contains non-valid character");
+			}
 		}
+
+		length = value.length();
+		reverse(value.begin(), value.end());
+		digits=value;
 
 	}
 	else {
+		//check that string is not "-"
 		if (len == 1) {
-			throw invalid_argument("invalid integer type");
+			throw invalid_argument("Invalid Integer Type: Not an Integer");
 		}
-		if ((value[1] - '0' > 10) || (value[1] - '0') < 0) {
-			throw invalid_argument("invalid integer type");
+		//first digit should not be '0'
+		if ((value[1] - '0' > 10) || (value[1] - '0') < 1) {
+			throw invalid_argument("Invalid Integer Type: Leading Zero");
+		}
+		//check that following digits are between 0 to 9
+		for(int i=2; i<len; i++){
+			if((value[i]-'0')>10 || (value[i]-'0')<0){
+				throw invalid_argument("Invalid Integer Type:Contains non-valid character");
+			}
 		}
 		length = value.length() - 1;
 		sign = 1;
-		for (int i = len - 1; i >= 1; i--) {
-			digits.push_back(value[i] - '0');
-		}
+		reverse(value.begin()+1, value.end());
+		digits=value.substr(1);
 	}
 
+}
 
+HugeInt::HugeInt(int s, string d){
+	sign=s;
+	digits=d;
+	length=d.length();
 }
 
 
 
-//return true if a > b
-//else return false
+//if a==b return 0
+//if a>b return 1
+//if a<b return -1
 
 int HugeInt::cmp(const HugeInt& b)const {
 	if (this->length > b.length) return 1;
@@ -77,150 +102,132 @@ int HugeInt::cmp(const HugeInt& b)const {
 }
 
 
-HugeInt HugeInt::addNum(const HugeInt& b)const {
-	HugeInt temp;
-	temp.sign = this->sign;
-	if (this->length > b.length) {
-		int carry = 0;
-		int len = b.length;
-		for (int i = 0; i < len; i++) {
-			int s = this->digits[i] + b.digits[i] + carry;
-			temp.digits.push_back(s % 10);
-			carry = s / 10;
-		}
-
-		for (int i = len; i < this->length; i++) {
-			int s = this->digits[i] + carry;
-			temp.digits.push_back(s % 10);
-			carry = s / 10;
-		}
-		if (carry > 0) temp.digits.push_back(carry);
-
+string HugeInt::addNum(const HugeInt& b)const {
+	string temp;
+	string s1=this->digits;
+	string s2=b.digits;
+	int len1=(int)s1.size();
+	int len2=(int)s2.size();
+	if (len1<len2){
+		swap(s1,s2);
+		swap(len1,len2);
 	}
-	else {
-		int carry = 0;
-		int len = this->length;
-		for (int i = 0; i < len; i++) {
-			int s = this->digits[i] + b.digits[i] + carry;
-			temp.digits.push_back(s % 10);
-			carry = s / 10;
-		}
-
-		for (int i = len; i < b.length; i++) {
-			int s = b.digits[i] + carry;
-			temp.digits.push_back(s % 10);
-			carry = s / 10;
-		}
-		if (carry > 0) temp.digits.push_back(carry);
+	int carry = 0;
+	for (int i = 0; i < len2; i++) {
+		int sa=s1[i]-'0';
+		int sb=s2[i]-'0';
+		int s = sa+sb + carry;
+		temp.push_back(char(s % 10+'0'));
+		carry = s / 10;
 	}
-	temp.length = temp.digits.size();
 
+	for (int i = len2; i < len1; i++) {
+		int sa=s1[i]-'0';
+		int s = sa + carry;
+		temp.push_back(char(s % 10+'0'));
+		carry = s / 10;
+	}
+	if (carry > 0) temp.push_back(char(carry+'0'));
 	return temp;
-
-
-
 }
 
-HugeInt HugeInt::subNum(const HugeInt& b)const {
+string HugeInt::subNum(const HugeInt& b)const {
 
 	int c = this->cmp(b);
-	if (c == 1) {
-		HugeInt temp;
-		int carry = 0;
-		temp.sign = this->sign;
-		int len = b.length;
-		for (int i = 0; i < len; i++) {
-			if (this->digits[i] >= (b.digits[i] + carry)) {
-				temp.digits.push_back(this->digits[i] - (b.digits[i] + carry));
-				carry = 0;
-			}
-			else {
-				int s = 10 + this->digits[i] - b.digits[i];
-				temp.digits.push_back(s);
-				carry = 1;
-			}
-		}
-
-		for (int i = len; i < this->length; i++) {
-			if (this->digits[i] >= carry) {
-				temp.digits.push_back(this->digits[i] - carry);
-				carry = 0;
-			}
-			else {
-				int s = 10 + this->digits[i] - carry;
-				temp.digits.push_back(s);
-				carry = 1;
-			}
-		}
-
-		temp.length = temp.digits.size();
-		return temp;
-
-	}
-	else if (c == 0) {
-		HugeInt temp(0);
-		temp.length = temp.digits.size();
+	if(c==0){
+		string temp="0";
 		return temp;
 	}
-
-	else {
-		HugeInt temp;
-		temp.sign = 1;
-		int len = this->length;
-		int carry = 0;
-		for (int i = 0; i < len; i++) {
-			if (b.digits[i] >= (this->digits[i] + carry)) {
-				temp.digits.push_back(b.digits[i] - (this->digits[i] + carry));
-				carry = 0;
-			}
-			else {
-				int s = 10 + b.digits[i] - this->digits[i];
-				temp.digits.push_back(s);
-				carry = 1;
-			}
-		}
-
-		for (int i = len; i < b.length; i++) {
-			if (b.digits[i] >= carry) {
-				temp.digits.push_back(b.digits[i] - carry);
-				carry = 0;
-			}
-			else {
-				int s = 10 + b.digits[i] - carry;
-				temp.digits.push_back(s);
-				carry = 1;
-			}
-		}
-		temp.length = temp.digits.size();
-		return temp;
-
+	string s1=this->digits;
+	string s2=b.digits;
+	int len1=(int)s1.size();
+	int len2=(int)s2.size();
+	if(c==-1){
+		swap(s1,s2);
+		swap(len1, len2);
 	}
+	
+	string temp;
+	int carry = 0;
+
+	for (int i = 0; i < len2; i++) {
+		int sa=s1[i]-'0';
+		int sb=s2[i]-'0';
+		if (sa >= (sb + carry)) {
+			temp.push_back(char(sa - (sb + carry)+'0'));
+			carry = 0;
+		}
+		else {
+			int s = 10 + sa - (sb+carry);
+			temp.push_back(char(s+'0'));
+			carry = 1;
+		}
+	}
+
+	for (int i = len2; i < len1; i++) {
+		int sa=s1[i]-'0';
+		if (sa >= carry) {
+			temp.push_back(char(sa - carry+'0'));
+			carry = 0;
+		}
+		else {
+			int s = 10 + sa - carry;
+			temp.push_back(char(s+'0'));
+			carry = 1;
+		}
+	}
+	//if the first digit is 0 remove it
+	while(temp.back()=='0'){
+		temp.pop_back();
+	}
+	
+	
+	return temp;
 }
 
 
 HugeInt HugeInt:: operator + (const HugeInt& arg) const {
 
 	if (this->sign == arg.sign) {
-		HugeInt temp = this->addNum(arg);
-		return temp;
-	}
-	else {
-		HugeInt temp = this->subNum(arg);
+		
+		string t=this->addNum(arg);
+		HugeInt temp(this->sign,t);
 		return temp;
 	}
 
+	else {
+		string t=this->subNum(arg);
+		if(this->cmp(arg)==-1){
+			HugeInt temp(arg.sign, t);
+			return temp;
+		}
+		else if(this->cmp(arg)==1){
+			HugeInt temp(this->sign, t);
+			return temp;
+		}
+		else{
+			HugeInt temp(0);
+			return temp;
+		}
+	}
 }
 
 HugeInt HugeInt:: operator * (const HugeInt& arg) const {
+	HugeInt zero(0);
+	if(this->cmp(zero)==0 || arg.cmp(zero)==0){
+		HugeInt tmp(0);
+		return tmp; 
+	}
 	int n = this->length;
 	int m = arg.length;
-	HugeInt temp(string(n * m + 3, '0'));
-	temp.sign = (this->sign != arg.sign) ? 1 : 0;
+	vector<int> temp(n * m + 3, 0);
 	int carry = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			int s = this->digits[i] * arg.digits[j];
-			temp.digits[i + j] += s;
+			int a=this->digits[i]-'0';
+			int b=arg.digits[j]-'0';
+			int s = a * b;
+			temp[i + j] += s;
 		}
 	}
 
@@ -228,8 +235,8 @@ HugeInt HugeInt:: operator * (const HugeInt& arg) const {
 	int s;
 
 	for (int i = 0; i < len; i++) {
-		s = temp.digits[i] + carry;
-		temp.digits[i] = s % 10;
+		s = temp[i] + carry;
+		temp[i] = s % 10;
 		carry = s / 10;
 	}
 
@@ -238,7 +245,7 @@ HugeInt HugeInt:: operator * (const HugeInt& arg) const {
 	int last = -1;
 
 	for (int i = n * m - 1; i >= 0; i--) {
-		if (temp.digits[i] != 0) {
+		if (temp[i] != 0) {
 			last = i;
 			break;
 		}
@@ -246,21 +253,27 @@ HugeInt HugeInt:: operator * (const HugeInt& arg) const {
 
 	//trim additional 0's
 	vector<int>::iterator it;
-	it = temp.digits.begin();
+	it = temp.begin();
 
 	if (last == -1) {
-		while (temp.digits.size() > 1) {
-			temp.digits.pop_back();
+		while ((int)temp.size() > 1) {
+			temp.pop_back();
 		}
 	}
 	else {
 		int cnt = len - last - 1;
 		while (cnt--) {
-			temp.digits.pop_back();
+			temp.pop_back();
 		}
 	}
-	temp.length = temp.digits.size();
-	return temp;
+	
+	string str;
+	for(int i=0; i<(int)temp.size(); i++){
+		str.push_back(char(temp[i]+'0'));
+	}
+	int tsign = (this->sign != arg.sign) ? 1 : 0;
+	HugeInt tmp(tsign,str);
+	return tmp;
 }
 
 ostream& operator<<(ostream& out, const HugeInt& a) {
